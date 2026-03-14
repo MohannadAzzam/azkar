fbbc23f1382c19ff4c98b6dac1ebb9bebf493d28
MohannadAzzam <mohannad.azzam2000@gmail.com>
2026-03-14 22:34:37 +0200

    add azkar file json and zikir model and pubspec image and json

---
+++ lib/data/models/zikir_category.dart
@@ -0,0 +1,31 @@
+class ZikirCategory {
+  final int id;
+  final String category;
+  final List<ZikirItem> content;
+
+  ZikirCategory({required this.id, required this.category, required this.content});
+
+  factory ZikirCategory.fromJson(Map<String, dynamic> json) {
+    return ZikirCategory(
+      id: json['id'],
+      category: json['category'],
+      content: (json['content'] as List).map((i) => ZikirItem.fromJson(i)).toList(),
+    );
+  }
+}
+
+class ZikirItem {
+  final String text;
+  final int count;
+  final String description;
+
+  ZikirItem({required this.text, required this.count, required this.description});
+
+  factory ZikirItem.fromJson(Map<String, dynamic> json) {
+    return ZikirItem(
+      text: json['text'],
+      count: json['count'],
+      description: json['description'],
+    );
+  }
+}
